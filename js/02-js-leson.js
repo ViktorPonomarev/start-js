4b308455d0c26124adfeb5bacba88c6ca346830f
ViktorPonomarev <viktorponomarev15.02.89@gmail.com>
2022-08-26 22:41:05 +0300

    Закончился урок

--- js/02-js-leson.js
+++ js/02-js-leson.js
@@ -277,13 +277,13 @@
 //і повертає фінальну суму із збереженою знижкою.
 
 
-function saveDiscount(discount) {
-  return function  (price) {
-    return price - (price * discount); 
-  }
-} 
-const callacDiscont = saveDiscount(0.1);
-console.log(callacDiscont(100));
+// function saveDiscount(discount) {
+//   return function  (price) {
+//     return price - (price * discount);
+//   }
+// }
+// const callacDiscont = saveDiscount(0.1);
+// console.log(callacDiscont(100));
 
 // !
 // Напишіть дві функції
@@ -389,6 +389,195 @@ console.log(callacDiscont(100));
 //Додай методи addNote(note), removeNote(text)
 //updatePriority(text, newPriority)
 
+// Задача 1
+// За допомогою методу forEach потрібно заповнити масив uploadedFiles
+// baseUrl потрібно передати, як this
+
+// const imagesName = ['car', 'grass', 'avatar', 'user', 'sea', 'water', 'bird'];
+// const baseUrl = 'domain-name.com';
+// let uploadedFiles = [];
+//
+// const resultUploadedFiles = [
+//   'domain-name.com/upload/car',
+//   'domain-name.com/upload/grass',
+//   'domain-name.com/upload/avatar',
+//   'domain-name.com/upload/user',
+//   'domain-name.com/upload/sea',
+//   'domain-name.com/upload/water',
+//   'domain-name.com/upload/bird'
+// ];
+
+// imagesName.forEach(function (item) {
+//   uploadedFiles.push(this + '/upload/' + item);
+// }, baseUrl);
 
 
 
+// Задача 2
+// Потрібно отримати URL адресу із адресного рядка бразуера.
+// Далі необхідно параметри запиту записати в об'єкт  (query) -
+// ключ, значення(axios)
+
+// url = https://www.npmjs.com/?query=axios&version=0.3&module=render&size=32&date=12212183311
+
+// const url = 'https://www.npmjs.com/?query=axios&version=0.3&module=render&size=32&date=12212183311';
+
+// const index = url.indexOf('?');
+// const newUrl = url.slice(index + 1);
+
+// const finalArr = newUrl.split('&').reduce((acc, item) => {
+//   const [key, value] = item.split('=');
+//   acc[key] = value;
+
+//   return acc;
+// }, {});
+
+// console.log(finalArr);
+
+
+// Задача 4
+
+// Напишіть функцію addItemToBasket, яка буде за допомогою методу find перевіряти, чи є вже такий продукт в кошику
+// Якщо він є, то потроібно оновити його в коризні
+// Якщо немає, то потрібно додати новий продукт до кошику покупок
+
+// Після цього порахувати суму всіх покупок і вивести total;
+
+// let total = 0;
+
+// const product1 = {
+//   id: 33,
+//   title: 'T-shirt',
+//   price: 99,
+//   count: 1,
+// }
+//
+// const product2 = {
+//   id: 29,
+//   title: 'Jeans',
+//   price: 199,
+//   count: 1,
+// }
+//
+//
+// const basketItems = [
+//   {
+//     id: 28,
+//     title: 'iPhone 14',
+//     price: 1599,
+//     count: 3,
+//   },
+//   {
+//     id: 29,
+//     title: 'Jeans',
+//     price: 199,
+//     count: 2,
+//   }
+// ];
+
+
+// Задача 5
+
+// Напишіть функцію, яка очищає масив від небажаних значень,
+// таких як false, undefined, порожні рядки, нуль, null.
+// Очікуваний результат: [0, 1, false, 2, undefined, '',
+// 3, null] => [1, 2, 3]
+
+
+// function cleanArray(arr) {
+// //  return arr.filter(item => !item === false);
+// // return arr.filter(item => item);
+// return arr.filter(Boolean);
+// }
+// console.log(cleanArray([0, 1, false, 2, undefined, '', 3, null]))
+
+
+// Задача 6
+
+
+// Перетворіть масив об’єктів виборців на підрахунок кількості людей, які проголосували
+
+// const voters = [
+//   {name:'Bob' , age: 30, voted: true},
+//   {name:'Jake' , age: 32, voted: true},
+//   {name:'Kate' , age: 25, voted: false},
+//   {name:'Sam' , age: 20, voted: false},
+//   {name:'Phil' , age: 21, voted: true},
+//   {name:'Ed' , age:55, voted:true},
+//   {name:'Tami' , age: 54, voted:true},
+//   {name: 'Mary', age: 31, voted: false},
+//   {name: 'Becky', age: 43, voted: false},
+//   {name: 'Joey', age: 41, voted: true},
+//   {name: 'Jeff', age: 30, voted: true},
+//   {name: 'Zack', age: 19, voted: false}
+// ];
+
+
+
+// function totalVotes(arr) {
+//   return arr.reduce((acc, {voted}) => {
+//     if ( voted ) {
+//       acc = acc + 1;
+//     }
+//      return acc;
+//   }, 0)
+ 
+// }
+
+// console.log(totalVotes(voters)); // 7
+
+
+
+// Отримайте ініціали імені
+// Даний рядок із кількох слів із одним пробілом між кожним із них.
+//  Скоротіть назву та поверніть ініціали імені.
+
+// const input = 'George Raymond Richard Martin';
+
+// // Result
+// // 'GRRM'
+ 
+
+// const res = input
+//   .split(' ')
+//   .map(item => item[0])
+//   .join('');
+// console.log(res);
+
+
+
+
+
+// Розробники люблять скорочувати все:
+//   k8s означає Kubernetes, a11y означає accessibility, l10n означає localization.
+//   Ви отримуєте нумероніми Dev, беручи першу та останню букви та підраховуючи кількість букв між ними.
+//   Слова, що містять менше 4 літер, не скорочуються, тому що це було б просто дивно.
+//   Змінна input  є реченням, і ви повинні скорочувати кожне слово довжиною 4 літери або більше.
+//   У реченні не буде розділових знаків. g2d l2k e6e
+
+
+
+// const input = 'Every developer likes to mix kubernetes and javascript';
+// Result
+// 'E3y d7r l3s to mix k8s and j8t'
+
+
+// const input = 'Every developer likes to mix kubernetes and javascript';
+// // Result
+// // 'E3y d7r l3s to mix k8s and j8t'
+
+// function calcLetters(word) {
+//   return word.length - 2;
+// }
+
+// const res = input
+//   .split(' ')
+//   .map(item => {
+//     if (item.length < 4) {
+//       return item;
+//     } else {
+//       return `${item[0]}${calcLetters(item)}${item[item.length - 1]}`;
+//     }
+//   })
+//   .join(' ');
+// console.log(res);
